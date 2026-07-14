9da6e8dcda7aa65db467e593138cae0371755104
Gabriel <gabrielkn254@gmail.com>
2026-07-14 13:15:59 +0300

    chore: create coffee class

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.size = size
+        self.price = price
+    
+    def __setattr__(self, name, value):
+        if name == "size":
+            if name != "Small" or "Medium" or "Large":
+                print("size must be Small, Medium, or Large")
+        
+        self.__dict__[name] = value
+    
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
