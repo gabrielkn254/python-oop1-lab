8872d8500855f1290eb290b0ee6db9fa12ee5b96
Gabriel <gabrielkn254@gmail.com>
2026-07-14 13:06:31 +0300

    chore: create book class

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    def __setattr__(self, name, value):
+        if name == "page_count":
+            if type(value) != int:
+                print("page_count must be an integer")
+                return
+        self.__dict__[name] = value
+
+    
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
     
         
